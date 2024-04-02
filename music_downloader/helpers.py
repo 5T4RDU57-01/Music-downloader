4931fd98e588b1fbe77f14d0f663a680d60c07cf
vivian <141859830+5T4RDU57-01@users.noreply.github.com>
2024-04-02 22:19:22 +0500

    Add files via upload
    used ffmpeg to convert the mp4 file into mp3 so that it is a valid mp3 file

--- music_downloader/helpers.py
+++ music_downloader/helpers.py
@@ -1,71 +1,74 @@
 from pytube import YouTube, Playlist, Search
 from re import match
 from os.path import basename, splitext, exists
-from os import rename
+from os import remove
+from subprocess import call
 
 
-class Downloader():
+class Downloader:
     def __init__(self, change_ext=True) -> None:
         self.change_ext = change_ext
 
     # Download a video with the given URL
     # Is also used in the download playlist function, the video object can be given as an arg
-    def download_video(self, url:str=None, youtube_object=None) -> int:
+    def download_video(self, url: str = None, youtube_object=None) -> int:
+
         try:
-            
             if url is not None:
                 video = YouTube(url)
-            
+
             elif youtube_object is not None:
                 video = youtube_object
-                
-            print(f'\nDownloading {video.title}')
-            
+
+            print(f"\nDownloading {video.title}")
+
             if self.change_ext:
                 stream = video.streams.filter(only_audio=True).first()
             else:
                 try:
-                    stream = video.streams.filter(file_extension="mp4").get_by_resolution("720p")
+                    stream = video.streams.filter(
+                        file_extension="mp4"
+                    ).get_by_resolution("720p")
                 except:
                     pass
-                
+
             if stream is None:
                 print(f"Could not download {video.title} in Mp4, downloading in mp3")
                 stream = video.streams.filter(only_audio=True).first()
-                
+
             downloaded_file = stream.download()
-            
+
             if self.change_ext:
                 self.change_extention_to_mp3(downloaded_file)
-            
-            print('Download successful!\n')
-            
+
         except KeyError:
             return 1
-        
+
+        print("Download successful!\n")
+
+
+
         return 0
 
-
     # Download all the videos in a playlist with the given URL
-    def download_playlist(self, url:str) -> int:
+    def download_playlist(self, url: str) -> int:
 
         try:
             pl = Playlist(url)
         except:
             return 1
-        
+
         for vid in pl.videos:
             self.download_video(youtube_object=vid)
-            
+
         return 0
-                    
 
     # Read a text file with the songnames (seperated by newlines)
     # and download all of them
     def download_from_file(self, file: str) -> int:
         songs = None
 
-        with open(file, 'r') as songfile:
+        with open(file, "r") as songfile:
             songs = songfile.readlines()
 
         for song in songs:
@@ -73,37 +76,40 @@ class Downloader():
 
         return 0
 
-
     # Search a song on youtube an download it
     def search_and_download(self, song: str) -> int:
-        
+
         result = (Search(f"{song} song").results)[0]
-    
+
         self.download_video(youtube_object=result)
         return 0
-
 
     # Chnage the extention of a file to MP3
     @staticmethod
     def change_extention_to_mp3(file: str) -> None:
 
+
         file = basename(file)
-
         base, ext = splitext(file)
-        new_file = base + '.mp3'
-        rename(file, new_file)
 
+        command = "ffmpeg -i {} -vn -ar 44100 -ac 2 -b:a 192k {}".format(f'"{file}"', f'"{base}.mp3"')
+        call(command, shell=True)
 
+        remove(file)
+
+        
 
 
 # Validate a youtube URL (can be playlist or video)
 def validate_url(url: str) -> bool:
-    matched = match(r'^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$', url)
+    matched = match(
+        r"^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$",
+        url,
+    )
 
-    return (matched != None)
+    return matched != None
 
 
 # See if a file exists and is a text file
-def validate_file(filename:str) -> bool:
-    return (exists(filename) and 
-            basename(filename).endswith(".txt"))
+def validate_file(filename: str) -> bool:
+    return exists(filename) and basename(filename).endswith(".txt")
--- music_downloader/main.py
+++ music_downloader/main.py
@@ -11,6 +11,7 @@ def main():
 
     return 0
 
+
 # Downloads the video, playlist, videos from a file or from a searh
 def do_everything(args: Namespace) -> int:
 
@@ -24,7 +25,7 @@ def do_everything(args: Namespace) -> int:
         else:
             return 1
     # <!VIDEO>
-        
+
     # <PLAYLIST>
     if args.playlist:
         if validate_url(args.playlist[0]):
@@ -32,7 +33,7 @@ def do_everything(args: Namespace) -> int:
         else:
             return 1
     # <!PLAYLIST>
-        
+
     # <FILE>
     if args.file:
         if validate_file(args.file[0]):
@@ -40,61 +41,71 @@ def do_everything(args: Namespace) -> int:
         else:
             return 2
     # <!FILE>
-        
+
     # <SEARCH>
     if args.download:
         down.search_and_download(args.download[0])
     # <!SEARCH>
 
-    return 0 
+    return 0
 
 
 # Return the namespace with all the given arguments
 def get_args() -> Namespace:
     parser = ArgumentParser()
 
-
     group = parser.add_mutually_exclusive_group(required=True)
 
     # <!VIDEO>
-    group.add_argument("-v", "--video", 
-                       help="Given a YouTube URL, download the video", 
-                       metavar=("url"), 
-                       nargs=1)
+    group.add_argument(
+        "-v",
+        "--video",
+        help="Given a YouTube URL, download the video",
+        metavar=("url"),
+        nargs=1,
+    )
     # <!VIDEO>
-    
+
     # <PLAYLIST>
-    group.add_argument("-p", "--playlist",
-                       help="Given a YouTube URL, download the playlist", 
-                       metavar=("url"), 
-                       nargs=1)
+    group.add_argument(
+        "-p",
+        "--playlist",
+        help="Given a YouTube URL, download the playlist",
+        metavar=("url"),
+        nargs=1,
+    )
     # <!PLAYLIST>
-    
+
     # <FILE>
 
-    group.add_argument("-f", "--file", 
-                       help="Given a text file, download all the songs in it (seperated by newlines)", 
-                       metavar="filename", 
-                       nargs=1)
+    group.add_argument(
+        "-f",
+        "--file",
+        help="Given a text file, download all the songs in it (seperated by newlines)",
+        metavar="filename",
+        nargs=1,
+    )
     # <!FILE>
 
     # <SEARCH>
-    group.add_argument("-d", "--download", 
-                       help="Given the name of a song, download it", 
-                       metavar=("songname"), 
-                       nargs=1)
+    group.add_argument(
+        "-d",
+        "--download",
+        help="Given the name of a song, download it",
+        metavar=("songname"),
+        nargs=1,
+    )
     # <!SEARCH>
 
     # <DOWNLOAD AS MP4>
-    parser.add_argument("-r", "--raw",
-                        help="Download the video as MP4",
-                        action="store_true",
-                        default=False
-                        )
+    parser.add_argument(
+        "-r",
+        "--raw",
+        help="Download the video as MP4",
+        action="store_true",
+        default=False,
+    )
     # <DOWNLOAD AS MP4>
-    
-
-
 
     args: Namespace = parser.parse_args()
 
@@ -104,9 +115,9 @@ def get_args() -> Namespace:
 # Print a message given a code
 def give_message(code: int) -> None:
     messages: dict = {
-        0 : "Action completed successfully!",
-        1 : "Incorrect URL, please fix thanxx <3",
-        2 : "File does not exist or is not a text file :3",
+        0: "Action completed successfully!",
+        1: "Incorrect URL, please fix thanxx <3",
+        2: "File does not exist or is not a text file :3",
     }
 
     print(messages[code])
